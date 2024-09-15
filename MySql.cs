f325349edfb5babf462b13add724a0e00579313e
MrVsps <xxxcamilo31@gmail.com>
2024-09-14 19:00:47 -0500

    inicio

--- MySql.cs
+++ MySql.cs
@@ -1,12 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace AdaptadorDB
 {
     internal class MySql
     {
+
+        internal class Consumo_CS
+        {
+            public int IdConsumo { get; set; }
+            public int IdEmpleado { get; set; }
+            public string TipoConsumo { get; set; }
+            public DateTime FechaRegistro { get; set; }
+            public Boolean FormaRegistro { get; set; }
+        }
+
+        internal class Empleado
+        {
+
+            public int IdEmpleado { get; set; }
+            public string NumeroDocumento { get; set; }
+            public string NombreCompleto { get; set; }
+            public string ZonaDeTrabajo { get; set; }
+            public int NumeroConsumos { get; set; }
+            public bool Estado { get; set; }
+            public DateTime FechaRegistro { get; set; }
+
+        }
+
+        public void AgregarConsumoCS(int idempleado, string tipoConsumo, DateTime fechaRegistro, bool registro)
+        {
+            using (MySqlConnection oconexion = new MySqlConnection("server=127.0.0.1;database=consumoempleado;uid=root;password=123456789;"))
+            {
+                try
+                {
+                    oconexion.Open();
+                    MySqlCommand command = oconexion.CreateCommand();
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.CommandText = "AgregarConsumoCS";
+                    command.Parameters.AddWithValue("@IdEmpleado", idempleado);
+                    command.Parameters.AddWithValue("@TipoConsumo", tipoConsumo);
+                    command.Parameters.AddWithValue("@FechaRegistro", fechaRegistro);
+                    command.Parameters.AddWithValue("@Registro", registro ? 1 : 0); // BIT en MySQL
+                    command.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    // Manejo de excepciones
+                }
+                finally
+                {
+                    oconexion.Close();
+                }
+            }
+        }
+
+        public void InsertarEmpleado(string numeroDocumento, string nombreCompleto, string zonaDeTrabajo, int consumos, bool estado, DateTime hoy)
+        {
+            using (MySqlConnection oconexion = new MySqlConnection("server=127.0.0.1;database=consumoempleado;uid=root;password=123456789;"))
+            {
+                oconexion.Open();
+                MySqlCommand command = oconexion.CreateCommand();
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "InsertarEmpleado";
+
+                command.Parameters.AddWithValue("@p_NumeroDocumento", numeroDocumento);
+                command.Parameters.AddWithValue("@p_NombreCompleto", nombreCompleto);
+                command.Parameters.AddWithValue("@p_ZonaDeTrabajo", zonaDeTrabajo);
+                command.Parameters.AddWithValue("@p_NumeroConsumos", consumos);
+                command.Parameters.AddWithValue("@p_Estado", estado);
+                command.Parameters.AddWithValue("@p_FechaRegistro", hoy);
+                command.ExecuteNonQuery();
+            }
+        }
     }
 }
--- SqlServer.cs
+++ SqlServer.cs
@@ -1,18 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using static AdaptadorDB.SqlServer;
 
 namespace AdaptadorDB
 {
     internal class SqlServer
     {
+        internal class ConsumoCS
+        {
+            public int IdConsumo { get; set; }
+            public int IdEmpleado { get; set; }
+            public string TipoConsumo { get; set; }
+            public DateTime FechaRegistro { get; set; }
+        }
+
         public List<ConsumoCS> ListarCs()
         {
             List<ConsumoCS> lista = new List<ConsumoCS>();
-            using (SqlConnection oconexion = new SqlConnection(Conexion.catena))
+            using (SqlConnection oconexion = new SqlConnection("Data Source=(local);Initial Catalog=ConsumoEmpleado;Integrated Security=True"))
             {
+                oconexion.Open();
                 try
                 {
                     string query = "exec ObtenerConsumoCS;";
@@ -41,6 +50,82 @@ namespace AdaptadorDB
             }
 
             return lista;
+        }
+
+
+        internal class Empleado
+        {
+            public int IdEmpleado { get; set; }
+            public string NumeroDocumento { get; set; }
+            public string NombreCompleto { get; set; }
+            public string ZonaDeTrabajo { get; set; }
+            public int NumeroConsumos { get; set; }
+            public string Imagen { get; set; }
+            public bool Estado { get; set; }
+            public DateTime FechaRegistro { get; set; }
+
+
+        }
+
+
+        public List<Empleado> Listar()
+        {
+            List<Empleado> lista = new List<Empleado>();
+            using (SqlConnection oconexion = new SqlConnection("Data Source=(local);Initial Catalog=ConsumoEmpleado;Integrated Security=True"))
+            {
+                oconexion.Open();
+                {
+                    try
+                    {
+                        string query = "select * from Empleado";
+                        SqlCommand cmd = new SqlCommand(query, oconexion);
+                        cmd.CommandType = CommandType.Text;
+                        oconexion.Open();
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                lista.Add(new Empleado
+                                {
+                                    IdEmpleado = Convert.ToInt32(reader["IdEmpleado"]),
+                                    NumeroDocumento = reader["NumeroDocumento"].ToString(),
+                                    NombreCompleto = reader["NombreCompleto"].ToString(),
+                                    ZonaDeTrabajo = reader["ZonaDeTrabajo"].ToString(),
+                                    NumeroConsumos = Convert.ToInt32(reader["NumeroConsumos"]),
+                                    Imagen = reader["Imagen"].ToString(),
+                                    Estado = Convert.ToBoolean(reader["Estado"]),
+                                    FechaRegistro = Convert.ToDateTime(reader["FechaRegistro"])
+                                });
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        lista = new List<Empleado>();
+
+                    }
+                    oconexion.Close();
+                }
+                return lista;
+            }
+        }
+
+    }
+    class ListarConsumoCS
+    {
+        private  SqlServer _queryConsumoCS = new SqlServer();
+        public List<ConsumoCS> Listar()
+        {
+            return _queryConsumoCS.ListarCs();
+        }
+    }
+
+    class listaruEmpleado
+    {
+        private SqlServer _queryEmpleado = new SqlServer();
+        public List<Empleado> Listar()
+        {
+            return _queryEmpleado.Listar();
         }
     }
 }
